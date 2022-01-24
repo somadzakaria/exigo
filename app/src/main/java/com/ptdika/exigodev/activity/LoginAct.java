243986c32b106f539556f1a60a43f66245fcf889
somadzakaria <somadzakaria82@gmail.com>
2022-01-24 17:53:39 +0700

    z

---
+++ app/src/main/java/com/ptdika/exigodev/activity/LoginAct.java
@@ -0,0 +1,16 @@
+package com.ptdika.exigodev.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ptdika.exigodev.R;
+
+public class LoginAct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
